1d1f26db0cbe361b6bdce9557ea324069a39a03a
Ya <toomanycreepers@mail.ru>
2024-12-26 16:52:06 +0500

    Initial commit

---
+++ src/test/java/permyakov/skorokhodov/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package permyakov.skorokhodov.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
